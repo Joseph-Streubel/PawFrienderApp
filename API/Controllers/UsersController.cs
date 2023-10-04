5f54ed8fe9be8a3b49a23d6c9abc4672fc7a0ea2
Joseph Streubel <joseph.streubel@gmail.com>
2023-10-04 17:19:00 -0500

    Authentication and Extensions

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,13 +1,13 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")] // /api/users
-public class UsersController : ControllerBase
+
+public class UsersController : BaseApiController
 {
     private readonly DataContext _context;
 
@@ -17,6 +17,7 @@ public class UsersController : ControllerBase
 
     }
 
+    
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
@@ -25,6 +26,7 @@ public class UsersController : ControllerBase
         return users;
     }
 
+    [Authorize]
     [HttpGet("{id}")] // /api/users/2
     public async Task<ActionResult<AppUser>> GetUser(int id)
     {
